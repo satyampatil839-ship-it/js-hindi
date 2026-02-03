cfc364bf940430232404eb5749884668966a2cd2
satyampatil839-ship-it <satyampatil839@gmail.com>
2026-02-03 06:48:16 +0000

    commit

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -9,5 +9,27 @@ let isloggedIn=false
 //null=>standalone value
 //symbol=>unique
 //object
-console.log(typeof name);
-console.log(typeof null)//object
+// console.log(typeof name);
+// console.log(typeof null)//object
+// let score="33"
+// let valuenumber=Number(score)
+// console.log(valuenumber)
+// console.log(typeof valuenumberde)
+// let score2="33ab"
+// let valuenumber2=Number(score2)
+// console.log(valuenumber2)
+// console.log(typeof valuenumber2)
+// let score3=true
+// let valuenumber3=Number(score3)
+// console.log(typeof valuenumber3)
+// console.log(score3)
+// let Log=1
+// let booleanislog=Boolean(Log)
+// console.log(booleanislog)
+// console.log(typeof booleanislog)
+//1=>true;0=>false
+//"hitesh"=>true ;  ""=>false
+let someNumber=33
+let stringnumber=String(someNumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
