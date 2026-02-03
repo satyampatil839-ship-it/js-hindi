139fbda0802f09364c3ea0081b412b594a42e8d5
satyampatil839-ship-it <satyampatil839@gmail.com>
2026-02-03 04:47:20 +0000

    commit

---
+++ basics/datatypes.js
@@ -0,0 +1,13 @@
+"use strict";//treat all js code as newer version
+let name="hitesh"
+let age= 18
+let isloggedIn=false
+//number =>2to power 53
+//bigint
+//string=>""
+//boolean=> true/false
+//null=>standalone value
+//symbol=>unique
+//object
+console.log(typeof name);
+console.log(typeof null)//object
---
+++ basics/var.js
@@ -0,0 +1,11 @@
+const accountid=749890
+let accountemail="satyampatil839@gmail.com"
+var accountpassword="swami"
+accountcity="nanded"
+let accountstate;
+accountemail="swati@gmail.com"
+accountpassword="6789"
+accountcity="mumbai"
+
+
+console.table([accountemail,accountpassword,accountpassword])
